3b0f03b6f8618f00e059ec5cf10f40f82c4d0e5c
kaveer <kaveer.rajcoomar@gmail.com>
2017-05-04 21:31:48 +0400

    Add project files.

---
+++ RetrieveFileAndSubFolder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetrieveFileAndSubFolder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RetrieveFileAndSubFolder retrieveFileFolder = new RetrieveFileAndSubFolder();
+            retrieveFileFolder.Run();
+        }
+    }
+}
---
+++ RetrieveFileAndSubFolder/RetrieveFileAndSubFolder.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetrieveFileAndSubFolder
+{
+    class RetrieveFileAndSubFolder
+    {
+        string basePath;
+        string logPath;
+
+        public RetrieveFileAndSubFolder()
+        {
+            if (!string.IsNullOrWhiteSpace(ConfigurationManager.AppSettings["BasePath"]))
+                    basePath = ConfigurationManager.AppSettings["BasePath"];
+
+            if (!string.IsNullOrWhiteSpace(ConfigurationManager.AppSettings["LogPath"]))
+                logPath = ConfigurationManager.AppSettings["LogPath"];
+        }
+
+        public void Run()
+        {
+            List<string> files = RetrieveFiles(basePath);
+
+            if (files != null)
+            {
+                foreach (var item in files)
+                {
+                    string logFile = Path.Combine(logPath, String.Format("File_{0:yyyy-MM-dd}.txt", DateTime.Now));
+
+                    using (StreamWriter writer = new StreamWriter(logFile, true))
+                    {
+                        writer.WriteLine(item);
+                    }
+                }
+            }
+        }
+
+        private List<String> RetrieveFiles(string basePath)
+        {
+            List<String> files = new List<String>();
+            try
+            {
+                foreach (string file in Directory.GetFiles(basePath))
+                {
+                    files.Add(file);
+                }
+                foreach (string directory in Directory.GetDirectories(basePath))
+                {
+                    files.AddRange(RetrieveFiles(directory));
+                }
+            }
+            catch (Exception excpt)
+            {
+                Console.WriteLine(excpt.Message);
+            }
+
+            return files;
+        }
+    }
+}
